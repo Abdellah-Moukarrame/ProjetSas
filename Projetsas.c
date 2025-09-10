544495ed4731f895c1bcbb4c8bcb812deb68deca
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-09-10 18:05:27 +0200

    creation de projetsas

---
+++ Projetsas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void Ajouteravion();
+void modifieravion();
+void Supprimeravion();
+void Afficheravion();
+void Rechercheravion();
+void Trieravion();
+
+void Menu(){
+    printf("======================MENU======================");
+    printf("1-Ajouter");
+} 
